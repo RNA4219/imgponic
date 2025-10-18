7dd4a855473e78a9d6fe28504af0010fa2099ca2
R_N_A <33271130+RNA4219@users.noreply.github.com>
2025-10-18 18:32:00 +0900

    Handle WebKit and JavaScriptCore pkg-config fallbacks

--- tools/pkg-config-webkit.sh
+++ tools/pkg-config-webkit.sh
@@ -7,29 +7,61 @@ if [[ -z "${real_pkg_config}" ]]; then
   exit 127
 fi
 
+declare -A mappings=(
+  ["javascriptcoregtk-4.0"]="javascriptcoregtk-4.1"
+  ["webkit2gtk-4.0"]="webkit2gtk-4.1"
+)
+
 args=("$@")
-needs_translation=false
-for arg in "${args[@]}"; do
-  if [[ "${arg}" == *"javascriptcoregtk-4.0"* ]]; then
-    needs_translation=true
+matched_keys=()
+for key in "${!mappings[@]}"; do
+  for arg in "${args[@]}"; do
+    if [[ "${arg}" == *"${key}"* ]]; then
+      matched_keys+=("${key}")
+      break
+    fi
+  done
+done
+
+if [[ "${#matched_keys[@]}" -eq 0 ]]; then
+  exec "${real_pkg_config}" "${args[@]}"
+fi
+
+all_present=true
+for key in "${matched_keys[@]}"; do
+  if ! "${real_pkg_config}" --exists "${key}"; then
+    all_present=false
     break
   fi
 done
 
-if [[ "${needs_translation}" == false ]]; then
-  exec "${real_pkg_config}" "${args[@]}"
-fi
-
-if "${real_pkg_config}" --exists javascriptcoregtk-4.0; then
+if [[ "${all_present}" == true ]]; then
   exec "${real_pkg_config}" "${args[@]}"
 fi
 
 translated_args=()
 for arg in "${args[@]}"; do
-  translated_args+=("${arg//javascriptcoregtk-4.0/javascriptcoregtk-4.1}")
+  translated="${arg}"
+  for key in "${matched_keys[@]}"; do
+    translated="${translated//${key}/${mappings[$key]}}"
+  done
+  translated_args+=("${translated}")
 done
 
-if "${real_pkg_config}" --exists javascriptcoregtk-4.1; then
+translated_keys=()
+for key in "${matched_keys[@]}"; do
+  translated_keys+=("${mappings[$key]}" )
+done
+
+all_translated_present=true
+for key in "${translated_keys[@]}"; do
+  if ! "${real_pkg_config}" --exists "${key}"; then
+    all_translated_present=false
+    break
+  fi
+done
+
+if [[ "${all_translated_present}" == true ]]; then
   exec "${real_pkg_config}" "${translated_args[@]}"
 fi
 
